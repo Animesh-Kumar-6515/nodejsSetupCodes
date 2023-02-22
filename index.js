4feeb421b2399d8f5022abf546121f9a6d0470d7
Animesh Kumar <81911506+Animesh-Kumar-6515@users.noreply.github.com>
2023-02-22 23:19:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var superheroes=require("superheroes");
+
+var mySuperheroname=superheroes.random();
+
+console.log(mySuperheroname);
+
+var supervillains=require("supervillains");
+var mySupervillainname=supervillains.random();
+
+console.log(mySupervillainname);
